f8a35cc440b51228752dac8f8a7e4d2e960431e1
BETHWEL <bethwelk85@gmail.com>
2023-05-21 18:16:35 +0300

    done

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,32 @@
-#!/usr/bin/env python3
-
 def happy_new_year():
-    # code goes here!
-    pass
+    countdown = ""
+    for counter in range(10, 0, -1):
+        countdown += str(counter) + "\n"
+    countdown += "Happy New Year!"
+    return countdown
+
+
+def fizzbuzz_printer():
+    result = ""
+    for num in range(1, 101):
+        result += fizzbuzz(num) + "\n"
+    return result
+
+def fizzbuzz(num):
+    if num % 3 == 0 and num % 5 == 0:
+        return "FizzBuzz"
+    elif num % 3 == 0:
+        return "Fizz"
+    elif num % 5 == 0:
+        return "Buzz"
+    else:
+        return str(num)
+
+def reverse_string(string):
+    reversed_str = ""
+    for i in range(len(string) - 1, -1, -1):
+        reversed_str += string[i]
+    return reversed_str
 
 def square_integers(int_list):
-    # code goes here!
-    pass
-
-def fizzbuzz():
-    # code goes here!
-    pass
+    return [num ** 2 for num in int_list]
